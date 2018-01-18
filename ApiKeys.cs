d9ea7f4d73cd413573ec2a8a701cdc4b9b2efd1f
Tom Soderling <hey_tommy@icloud.com>
2018-01-18 02:48:56 -0600

    ApiKeys file

---
+++ ApiKeys.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Spaniel
+{
+    // Changes to this file are ignored so API keys won't be checked in to github. Used command:
+    // git update-index --assume-unchanged [path to file]
+    // To undo:
+    // git update-index --no-assume-unchanged [path to file]
+    public static class ApiKeys
+    {
+        public const string AppCenterAppSecret_iOS = "[your iOS App Center secret goes here]";
+        public const string AppCenterAppSecret_Android = "[your Android App Center secret goes here]";
+    }
+}
