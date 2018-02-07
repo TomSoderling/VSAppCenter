ba3a10089c2b980659c97483f8db10c07d6c1ca0
Tom Soderling <hey_tommy@icloud.com>
2018-02-07 07:58:18 -0600

    Update ApiKeys.cs
    Better directions on how to use this file

--- ApiKeys.cs
+++ ApiKeys.cs
@@ -1,9 +1,17 @@
 ﻿
 namespace Spaniel
 {
-    // Changes to this file are ignored so API keys won't be checked in to github. Used command:
-    // git update-index --assume-unchanged [path to file]
-    // To undo:
+    // Changes to this file are ignored so API keys won't be checked in to source control for the bots to exploit. 
+    // Directions:
+    // 1.) Commit this dummy version of the file
+    //     - Add this file to your project (in your Xamarin.Forms PCL or Shared Project)
+    //     - Commit and push in this state (with placeholder text)
+    // 2.) Mark this file as Assume Unchanged so we can make changes to it that git ignores
+    //     - In terminal, cd to the root of your project folder
+    //     - Run command: git update-index --assume-unchanged [path to file]
+    // 3.) Then add your real keys to this file. Any changes will be ignored now by git.
+    //
+    // If for some reason you need to undo this, just run this command:
     // git update-index --no-assume-unchanged [path to file]
     public static class ApiKeys
     {
