d6ff730dc9e6673e26b5060eb127bfd9954d2536
Tom Soderling <18268845+TomSoderling@users.noreply.github.com>
2020-05-03 22:41:13 -0500

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -2,32 +2,132 @@
 
 # Post Build Script
 
-set -e # Exit immediately if a command exits with a non-zero status (failure)
+# set -e # Exit immediately if a command exits with a non-zero status (failure)
+# don't set here. This causes the script to fail on this line: anyFailedTests=$(grep 'result=\"Failed\"' $pathOfTestResults)
 
 echo "**************************************************************************************************"
 echo "Post Build Script"
 echo "**************************************************************************************************"
 
+
 ##################################################
-# Start UI Tests
+# Run Unit Tests
 ##################################################
+
+# Run all NUnit test projects that have "UnitTest" in the name.
+# The script will build, run, and display the results in the build logs.
+
+echo 
+echo "**************************************************************************************************"
+echo "Run Unit Tests"
+echo "**************************************************************************************************"
+echo "Working directory:" $APPCENTER_SOURCE_DIRECTORY
+echo "**************************************************************************************************"
+echo 
+
+echo "Found NUnit test projects:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTest.*\.csproj' -exec echo {} \;
+echo
+
+echo "Building NUnit test projects:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTest.*\.csproj' -exec msbuild {} \;
+echo
+
+echo "Compiled projects to run NUnit tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UnitTest.*\.dll' -exec echo {} \;
+echo
+
+echo "Running NUnit tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UnitTest.*\.dll' -exec nunit3-console {} \;
+echo
+
+echo "Find NUnit test results:"
+pathOfTestResults=$(find $APPCENTER_SOURCE_DIRECTORY -name 'TestResult.xml')
+echo $pathOfTestResults
+echo
+
+# Check if file path variable is empty
+if [ -z "$pathOfTestResults" ]
+then
+    echo "NUnit test result file not found. Exiting."
+    exit 1 # exit with unspecified error code. Should be obvious why we can't continue the script
+else
+    echo "NUnit test result file found"
+fi
+
+# echo "Output NUnit test results:"
+# cat $pathOfTestResults
+# echo
+# echo
+
+echo "Look for failing tests:"
+anyFailedTests=$(grep 'result=\"Failed\"' $pathOfTestResults)
+
+if [ -z "$anyFailedTests" ]
+then
+    echo "All unit tests passed" 
+else
+    echo "One or more unit tests failed" 
+    echo $anyFailedTests
+    exit 1
+fi
+
+
+
+
+##################################################
+# Run UI Tests in Test Cloud
+##################################################
+
+set -e # Exit immediately if a command exits with a non-zero status (failure)
 
 # variables
-appCenterLoginApiToken=$AppCenterLoginForAutomatedUITests # this comes from the build environment variables
-appName="tomso/Pickster"
-deviceSetName="tomso/top-devices"
-testSeriesName="all-tests"
+shouldRunUITestsInTestCloud=$RunUITestsInTestCloud # yes/no flag
+appCenterLoginApiToken=$AppCenterLoginForAutomatedUITests # these all come from the build environment variables
+appName="tomso/Pickster-2"
+deviceSetName=$UITestDeviceSet
+testSeriesName="smoke-tests"
 
-echo ""
-echo "Start Xamarin.UITest run"
-echo "   App Name: $appName"
-echo " Device Set: $deviceSetName"
-echo "Test Series: $testSeriesName"
-echo ""
+echo 
+echo "**************************************************************************************************"
+echo "Run Xamarin.UITests in Test Cloud"
+echo "**************************************************************************************************"
+echo "Source directory: $APPCENTER_SOURCE_DIRECTORY"
+echo "Output directory: $APPCENTER_OUTPUT_DIRECTORY"
+echo "    Run UI Tests: $RunUITestsInTestCloud"
+echo "        App Name: $appName"
+echo "      Device Set: $deviceSetName"
+echo "     Test Series: $testSeriesName"
+echo 
 
-echo "> Run UI test command"
-# Note: must put a space after each parameter/value pair
-appcenter test run uitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/Pickster.ipa --test-series $testSeriesName --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Pickster.UITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools --token $appCenterLoginApiToken 
+if [ $shouldRunUITestsInTestCloud != "yes" ]
+then
+    echo "Should run UI tests in Test Cloud:" $shouldRunUITestsInTestCloud
+else
+    echo "> UI test command to run:"
+    echo "appcenter test run uitest" 
+    echo "--app $appName" 
+    echo "--devices $deviceSetName"
+    echo "--app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa"
+    echo "--test-series $testSeriesName"
+    echo "--locale \"en_US\"" 
+    echo "--build-dir $APPCENTER_SOURCE_DIRECTORY/Pickster/Pickster.UITests/bin/Debug"
+    echo "--uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/Pickster/packages/Xamarin.UITest.*/tools"
+    echo "--token $appCenterLoginApiToken"
+
+    echo ""
+    echo "> Run UI test command"
+    # Note: must put a space after each parameter/value pair
+    appcenter test run uitest \
+    --app $appName \
+    --devices $deviceSetName \
+    --app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa \
+    --test-series $testSeriesName \
+    --locale "en_US" \
+    --build-dir $APPCENTER_SOURCE_DIRECTORY/Pickster/Pickster.UITests/bin/Debug \
+    --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/Pickster/packages/Xamarin.UITest.*/tools \
+    --token $appCenterLoginApiToken 
+fi
 
 echo ""
 echo "**************************************************************************************************"
